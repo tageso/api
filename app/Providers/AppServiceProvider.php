12dac6a18ce6715c3dcf4ad65a71c9b1b2d48891
soeren <hello@kekskurse.de>
2018-11-21 14:39:37 +0100

    MYSQL Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -34,5 +35,10 @@ class AppServiceProvider extends ServiceProvider
             ]);
             return $s3;
         });
+    }
+
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
     }
 }
