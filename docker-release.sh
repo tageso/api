83da741c2df2afd37fa0152055b01f68d7931cb1
soeren <hello@kekskurse.de>
2018-11-22 10:16:44 +0100

    With all vendor stuff

--- docker-release.sh
+++ docker-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" -p --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 #rm -r -f vendor
 #composer install --no-dev
 docker build -t tageso/api .
