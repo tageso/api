50e10379b8be4b56bd27020a961932fbcde538b6
soeren <hello@kekskurse.de>
2018-11-23 14:40:40 +0100

    Release

--- docker-release.sh
+++ docker-release.sh
@@ -3,6 +3,7 @@ git reset --hard master
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 rm -r -f vendor
 composer install --no-dev
+docker pull tageso/api
 docker build -t tageso/api .
 docker push tageso/api
 rm -r -f vendor
