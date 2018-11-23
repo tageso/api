be6daec4f44724815d0f7a95fee0785acfebed7f
soeren <hello@kekskurse.de>
2018-11-23 13:30:59 +0100

    Install composer

--- docker-release.sh
+++ docker-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+rm -r -f vendor
 composer install --no-dev
 docker build -t tageso/api .
 docker push tageso/api
